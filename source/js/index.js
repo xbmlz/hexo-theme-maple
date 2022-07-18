4713b6b588d8f60ac58cb0b97a99b8f271cdc146
Petrichor <997909544@qq.com>
2022-07-18 22:07:49 +0800

    fix: darkmode bug

--- source/js/index.js
+++ source/js/index.js
@@ -1,57 +1,50 @@
 
 window.onload = () => {
   let media = window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)')
-  const setting = localStorage.getItem('hexo-color-scheme') || 'auto' //
   let isDark = () => {
-    let prefersDark = media.matches;
-    // if (setting === 'dark' || (prefersDark && setting !== 'light')) {
-    //   console.log('setting: ' + setting + ' and prefersDark: ' + prefersDark)
-    //   return true
-    // } else {
-    //   console.log('setting: ' + setting + ' and prefersDark: ' + prefersDark)
-    //   return false
-    // }
-    console.log("浏览器", prefersDark)
-    console.log("设置", setting === 'dark')
-    if ((setting === 'dark' && prefersDark) || (setting === 'light' && !prefersDark) || setting === 'auto') {// 都是暗黑模式或者都是白天模式
-      console.log("跟随浏览器", prefersDark)
-      return prefersDark // 跟随浏览器系统
-    } else { // 两者不一样
-      console.log("跟随设置", setting === 'dark')
-      return setting === 'dark' // 跟随设置
+    const setting = localStorage.getItem('hexo-color-scheme') || 'auto'
+    if (setting === 'dark' || (media.matches && setting !== 'light')) {
+      return true
+    } else{
+      return false
     }
-
   }
   // 切换为深色模式
   let toggleDarkMode = () => {
     document.documentElement.classList.toggle('dark', true)
     document.getElementById('moon-icon').style.display = 'block'
     document.getElementById('sun-icon').style.display = 'none'
-    localStorage.setItem('hexo-color-scheme', 'dark')
+    setDarkMode('dark')
   }
   // 切换为浅色模式
   let toggleLightMode = () => {
     document.documentElement.classList.toggle('dark', false)
     document.getElementById('moon-icon').style.display = 'none'
     document.getElementById('sun-icon').style.display = 'block'
-    localStorage.setItem('hexo-color-scheme', 'light')
+    setDarkMode('light')
+  }
+  let setDarkMode = (scheme) => {
+    const darkMode = media.matches ? 'dark' : 'light'
+    if (darkMode !== scheme) {
+      localStorage.setItem('hexo-color-scheme', scheme)
+    } else{
+      localStorage.setItem('hexo-color-scheme', 'auto')
+    }
   }
   // 切换为自动模式
   let updateMode = (e) => {
-    if (isDark()) {
-      console.log('浏览器是暗黑模式',)
-      document.documentElement.classList.toggle('dark', true)
-      document.getElementById('moon-icon').style.display = 'block'
-      document.getElementById('sun-icon').style.display = 'none'
-      /* toggleDarkMode() */
-    } else {
-      document.documentElement.classList.toggle('dark', false)
-      document.getElementById('moon-icon').style.display = 'none'
-      document.getElementById('sun-icon').style.display = 'block'
-      /* toggleLightMode() */
+    const setting = localStorage.getItem('hexo-color-scheme') || 'auto'
+    console.log(setting, e.matches)
+    if (setting === 'auto') {
+      if (e.matches) {
+        toggleDarkMode()
+      } else {
+        toggleLightMode()
+      }
     }
   }
 
+  // let media = getMedia()
   if (typeof media.addEventListener === 'function') {
     media.addEventListener('change', updateMode)
   } else if (typeof media.addListener === 'function') {
@@ -63,6 +56,7 @@ window.onload = () => {
   } else {
     document.getElementById('sun-icon').style.display = 'block'
   }
+
   document.getElementById('moon-icon').addEventListener('click', () => {
     toggleLightMode()
   })
